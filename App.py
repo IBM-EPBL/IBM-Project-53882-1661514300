00a28592e54b645d0efb1963f76d32c9f17c954b
amsavalli786 <113347062+amsavalli786@users.noreply.github.com>
2022-09-29 12:31:08 +0530

    AMSAVALLI

---
+++ App.py
@@ -0,0 +1,19 @@
+from flask import flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('inedex.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('/signin.html')
